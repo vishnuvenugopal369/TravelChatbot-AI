64c0c19884a8cf642eaa52b4caa3f672718875e9
vishnuvenugopal369 <vishnuvnambiar8277@gmail.com>
2026-03-13 16:01:10 +0530

    flight_search.py

---
+++ flight_search.py
@@ -0,0 +1,9 @@
+def search_flights(origin, destination, date):
+
+    flights = [
+        {"airline": "Lufthansa", "time": "08:30"},
+        {"airline": "Ryanair", "time": "12:15"},
+        {"airline": "EasyJet", "time": "18:45"},
+    ]
+
+    return flights
